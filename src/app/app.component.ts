b31fdbd165ace5327339863f04cf1b741cae97ae
wszgrcy <wszgrcy@gmail.com>
2024-07-28 17:12:28 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Root, createRoot } from 'react-dom/client';
+import Editor from './editor';
+import React from 'react';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng18-test';
+  anchor = viewChild.required<ElementRef<HTMLElement>>('anchor');
+  ngOnInit(): void {
+    let root = createRoot(this.anchor().nativeElement);
+    root.render(React.createElement(Editor));
+  }
 }
